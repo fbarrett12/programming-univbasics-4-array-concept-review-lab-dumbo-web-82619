2814059616aa9a5b13e15294ad7632c0609df4b7
Fredrick Barrett <f.barrett12@gmail.com>
2019-08-23 16:58:09 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,7 +3,10 @@ require "pry"
 def find_element_index(array, value_to_find)
   counter = 0 
   while counter < array.length do 
-    binding.pry 
+    #binding.pry 
+    if array[counter] == value_to_find
+      return counter
+  end
     counter += 1
   end 
 end
