25454a267217e51409814c77b465a095e7141bfe
Fredrick Barrett <f.barrett12@gmail.com>
2019-08-23 16:47:28 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,9 +3,7 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  new_array = []
-  new_array << array.sort.reverse 
-  return new_array.first
+ return array.sort.reverse.first
 end
 
 def find_min_value(array)
